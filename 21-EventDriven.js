b9da29183365d1548b1ea21e64f9db73a8899ec8
Krishna <skvkrishnamurthy@gmail.com>
2021-08-03 10:47:18 +0530

    Event driven programming

---
+++ 21-EventDriven.js
@@ -0,0 +1,19 @@
+// Event Driven programming
+/* const EventEmmiter = require('events')
+
+const customEmmiter = new EventEmmiter()
+
+// on -> listen for an event
+// emit -> emit an event
+customEmmiter.on('Response', (name, id) => {
+  console.log(`Data recieved user:${name} with ID:${id}`);
+})
+customEmmiter.on('Response', () => {
+  console.log('The data is pre-processed');
+})
+
+// customEmmiter.emit('Response')
+// the emit should be properly kept in place
+
+// passing argumets when emmitting
+customEmmiter.emit('Response','John', 24) */
