7cc8bf5050167529eb6c2c5a935b57bc71623dcb
Krishna <skvkrishnamurthy@gmail.com>
2021-08-03 11:04:19 +0530

    JS Important Topics

---
+++ app.js
@@ -0,0 +1,6 @@
+// Streams - { Continous source or big files}
+// -> Writeable - used to write data sequentially
+// -> readable - used to read data sequentially
+// -> Duplex - both read and write sequentially
+// -> Transform - where data can be modified when reading or writing
+
