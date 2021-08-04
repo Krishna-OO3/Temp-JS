ed7358e0c17b3ac4b8475f87d193f136939b1e47
Krishna <skvkrishnamurthy@gmail.com>
2021-08-04 16:43:13 +0530

    Completed Basics of node

--- app.js
+++ app.js
@@ -4,16 +4,16 @@
 // -> Duplex - both read and write sequentially
 // -> Transform - where data can be modified when reading or writing
 
-const { createReadStream } = require('fs')
+// const { createReadStream } = require('fs')
 
 // default 64kb
 // last buffer - remainder
 // highWaterMark - control size
 // const stream = createReadStream('./content/big.txt', { highWaterMark: 90000 })
 // const stream = createReadStream('../content/big.txt', { encoding: 'utf8' })
-const stream = createReadStream('./content/big.txt')
+// const stream = createReadStream('./content/big.txt')
 
-stream.on('data', (result) => {
-  console.log(result)
-})
-stream.on('error', (err) => console.log(err))
+// stream.on('data', (result) => {
+//   console.log(result)
+// })
+// stream.on('error', (err) => console.log(err))
